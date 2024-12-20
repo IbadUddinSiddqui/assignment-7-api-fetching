e57a6c7c70b92768a375821d5e662f11114fad7e
Ibad Uddin <ibaduddinsiddiqui418@gmail.com>
2024-12-20 18:42:32 +0500

    add server and client side fetched pages

---
+++ app/client-side/page.tsx
@@ -0,0 +1,73 @@
+"use client";
+import React, { useEffect, useState } from 'react';
+
+interface Products {
+  id: number;
+  title: string;
+  price: number;
+  description: string;
+  category: string;
+  image: string;
+  rating: {
+    rate: number;
+    count: number;
+  };
+}
+
+const Page: React.FC = () => {
+  const [products, setProducts] = useState<Products[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const response = await fetch('https://fakestoreapi.com/products');
+        const fetchedData: Products[] = await response.json();
+        setProducts(fetchedData);
+        setLoading(false);
+      } catch (error) {
+        console.error('Error fetching products:', error);
+        setLoading(false);
+      }
+    };
+
+    fetchProducts();
+  }, []); 
+  return (
+    
+    
+    <div className='bg-gray-900 mt-0'>
+       
+      <h1 className='text-5xl  text-white m-4 font-bold text-center '>Client-side Fetched Products</h1>
+    <p className='text-center'>  {loading ? "Fetching Data....." : ""}</p>
+      <ul className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3'>
+      {products.map((product) => (
+    <li
+      key={product.id}
+      className="flex flex-col bg-gray-900 items-center mx-auto md:w-[375px] w-[300px] lg:w-[450px] m-2 bg-gradient-to-r from-gray-800 to-gray-900 text-white border border-gray-700 rounded-lg shadow-lg transform transition duration-300 hover:scale-105 hover:shadow-2xl hover:border-blue-500"
+    >
+      <div className="w-full h-48 overflow-hidden rounded-t-lg">
+        <img
+          src={product.image}
+          alt={product.title}
+          className="w-full h-full object-contain p-4 hover:scale-110 transition-transform duration-300"
+        />
+      </div>
+      <div className="p-4 flex flex-col items-center space-y-2">
+        <h2 className="text-lg font-semibold text-center">{product.title}</h2>
+        <p className="text-sm text-gray-400 text-center">{product.description}</p>
+        <p className="text-lg font-bold text-blue-400">
+          ${product.price}
+        </p>
+      </div>
+      <button className="w-full bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-b-lg transition-all duration-300">
+        Add to Cart
+      </button>
+    </li>
+  ))}
+      </ul>
+    </div>
+     );
+};
+
+export default Page;
